3efc9661442b0c379a10c7f5e3e358e592c3cd86
kazu121212 <67034332+kazu121212@users.noreply.github.com>
2022-02-15 09:23:12 +0900

    itemモデルにimageが空では登録できないを追加

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -12,5 +12,9 @@ FactoryBot.define do
     price                    {Faker::Number.number(digits: 4)}
     
     association :user 
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -25,6 +25,13 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Explanation can't be blank")
       end
+
+      it 'imageが空では登録できない' do
+        @item.image = nil
+        @item.valid?
+        binding.pry
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
       it 'category_idが空では登録できない' do
         @item.category_id = ''
         @item.valid?
